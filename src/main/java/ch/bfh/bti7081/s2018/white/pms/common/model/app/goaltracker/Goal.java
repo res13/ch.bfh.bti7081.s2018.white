1b69b184a1dd238f13409526c2db9ab653cac79c
res13 <andreas.erb@gmx.ch>
2018-04-25 09:35:21 +0200

    corrected goal generalization

--- src/main/java/ch/bfh/bti7081/s2018/white/pms/common/model/app/goaltracker/Goal.java
+++ src/main/java/ch/bfh/bti7081/s2018/white/pms/common/model/app/goaltracker/Goal.java
@@ -1,10 +1,11 @@
 package ch.bfh.bti7081.s2018.white.pms.common.model.app.goaltracker;
 
+import ch.bfh.bti7081.s2018.white.pms.common.model.PmsType;
 import ch.bfh.bti7081.s2018.white.pms.common.model.user.User;
 
 import java.time.LocalDateTime;
 
-public class Goal {
+public class Goal extends PmsType {
 
     private LocalDateTime created;
 
