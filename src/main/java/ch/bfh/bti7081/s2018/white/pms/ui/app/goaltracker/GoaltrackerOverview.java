c6268f423aad43f05a335382f8a1a74bbd7e1eec
projektins <projekt.ins@gmail.com>
2018-05-04 15:13:16 +0200

    first Basic View for Goals

---
+++ src/main/java/ch/bfh/bti7081/s2018/white/pms/ui/app/goaltracker/GoaltrackerOverview.java
@@ -0,0 +1,98 @@
+package ch.bfh.bti7081.s2018.white.pms.ui.app.goaltracker;
+
+import ch.bfh.bti7081.s2018.white.pms.common.model.app.goaltracker.Goal;
+import ch.bfh.bti7081.s2018.white.pms.services.impl.GoalServiceImpl;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.ValueChangeMode;
+import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
+
+import javax.servlet.annotation.WebServlet;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GoaltrackerOverview extends UI {
+
+    private GoalServiceImpl goalService = new GoalServiceImpl();
+    private Grid<Goal> grid = new Grid<>(Goal.class);
+    private TextField filterText = new TextField();
+    private GoalView form = new GoalView(this);
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        Goal goal = new Goal();
+        goal.setGoal("abc");
+        goal.setCreated(LocalDateTime.now());
+
+        try {
+            goalService.saveOrUpdateEntity(goal);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        final VerticalLayout layout = new VerticalLayout();
+
+        filterText.setPlaceholder("filter by name...");
+        filterText.addValueChangeListener(e -> updateList());
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button clearFilterTextBtn = new Button(FontAwesome.TIMES);
+        clearFilterTextBtn.setDescription("Clear the current filter");
+        clearFilterTextBtn.addClickListener(e -> filterText.clear());
+
+        CssLayout filtering = new CssLayout();
+        filtering.addComponents(filterText, clearFilterTextBtn);
+        filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+
+        Button addCustomerBtn = new Button("Add new customer");
+        addCustomerBtn.addClickListener(e -> {
+            grid.asSingleSelect().clear();
+            form.setGoal(new Goal());
+        });
+
+        HorizontalLayout toolbar = new HorizontalLayout(filtering, addCustomerBtn);
+
+        grid.setColumns("goal", "goal", "goal");
+
+        HorizontalLayout main = new HorizontalLayout(grid, form);
+        main.setSizeFull();
+        grid.setSizeFull();
+        main.setExpandRatio(grid, 1);
+
+        layout.addComponents(toolbar, main);
+
+        // fetch list of Customers from goalService and assign it to Grid
+        updateList();
+
+        setContent(layout);
+
+        form.setVisible(false);
+
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            if (event.getValue() == null) {
+                form.setVisible(false);
+            } else {
+                form.setGoal(event.getValue());
+            }
+        });
+    }
+
+    public void updateList() {
+        List<Goal> goals = null;
+        try {
+            goals = goalService.getAllEntities();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        grid.setItems(goals);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = GoaltrackerOverview.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
